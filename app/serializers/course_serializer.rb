b9ee49b6533012da4ac0f6376ab6c6502902f828
Pete Hanner <petehanner@gmail.com>
2019-11-06 17:01:47 -0500

    course serializing

--- app/serializers/course_serializer.rb
+++ app/serializers/course_serializer.rb
@@ -3,13 +3,15 @@ class CourseSerializer < ActiveModel::Serializer
 
   def assignments
 
-    self.object.assignments.map do |asg|
-      {
-        description: asg.description,
-        og_date: asg.og_date,
-        adj_date: asg.adj_date,
-        course_id: asg.course_id
-      }
-    end
+    Assignment.date_grouped(self.object.assignments)
+
+    # self.object.assignments.map do |asg|
+    #   {
+    #     description: asg.description,
+    #     og_date: asg.og_date,
+    #     adj_date: asg.adj_date,
+    #     course_id: asg.course_id
+    #   }
+    # end
   end
 end
