3f730bbf61fd7f79f9d4cf6288cd1453b19958f9
Pete Hanner <petehanner@gmail.com>
2019-11-06 19:44:25 -0500

    working: serializing date-grouped assignments in their controller

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -1,5 +1,10 @@
 class AssignmentsController < ApplicationController
   def index
-    render json: Assignment.user_asgmts(1)
+
+    serialized = Assignment.date_grouped(Assignment.user_asgmts(1), :adj_date).transform_values do |asg_arr|
+      asg_arr.map { |asg| asg.slice(:id, :description, :og_date, :adj_date, :course_id) }
+    end
+
+    render json: serialized
   end
 end
--- app/serializers/course_serializer.rb
+++ app/serializers/course_serializer.rb
@@ -6,14 +6,5 @@ class CourseSerializer < ActiveModel::Serializer
     Assignment.date_grouped(self.object.assignments, :og_date).transform_values do |asg_arr|
       asg_arr.map { |asg| asg.slice(:id, :description, :og_date, :adj_date, :course_id) }
     end
-
-    # self.object.assignments.map do |asg|
-    #   {
-    #     description: asg.description,
-    #     og_date: asg.og_date,
-    #     adj_date: asg.adj_date,
-    #     course_id: asg.course_id
-    #   }
-    # end
   end
 end
