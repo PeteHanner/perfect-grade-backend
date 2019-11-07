9556b7c7090ae964f524d8a04f790ca01b62a134
Pete Hanner <petehanner@gmail.com>
2019-11-07 12:56:37 -0500

    course nested under assignments, need to clean up that data

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -3,27 +3,17 @@ class AssignmentsController < ApplicationController
 
     Assignment.flatten(User.find(1).assignments)
 
-    # puts "Finding assignments"
     asgs = User.find(1).assignments
-    # puts "Found assignments"
-    # puts "Grouping by date"
     grouped = Assignment.date_grouped(asgs, :adj_date)
-    # puts "Grouped by date"
-    # serialized = Assignment.date_grouped(User.find(1).assignments, :adj_date)
 
     puts "Serializing data"
     serialized = grouped.transform_values do |asg_arr|
-      asg_arr.map { |asg| asg.slice(:id, :description, :og_date, :adj_date, :course_id) }
+      asg_arr.map { |asg| asg.slice(:id, :description, :og_date, :adj_date, :course) }
     end
-    # puts "Data serialized"
 
-    # puts "Sorting for JSON delivery"
     sorted = serialized.sort_by{|k,v| k }
-    # puts "Done sorting, delivering JSON"
 
 
-    # render json: User.find(1).assignments
-    # render json: Assignment.flatten(User.find(1).assignments)#.sort_by{|k,v| k }
     render json: sorted
   end
 end
--- app/serializers/assignment_serializer.rb
+++ app/serializers/assignment_serializer.rb
@@ -1,3 +1,3 @@
 class AssignmentSerializer < ActiveModel::Serializer
-  attributes :id, :description, :og_date, :adj_date, :course_id
+  attributes :id, :description, :og_date, :adj_date
 end
