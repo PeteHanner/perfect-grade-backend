f9b2e62999c50f4dafc3ac6208c034bd6b88b04f
Pete Hanner <petehanner@gmail.com>
2019-11-07 17:36:02 -0500

    create course controller done

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,6 +1,14 @@
 class CoursesController < ApplicationController
   def index
-    # render json: Course.all
     render json: User.find(1).courses
+  end
+
+  def create
+    # puts params[:courseTitle]
+    newCourse = Course.create(
+      name: params[:courseTitle],
+      user: User.find(params[:userId])
+    )
+    render json: newCourse
   end
 end
