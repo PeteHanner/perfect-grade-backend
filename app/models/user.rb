d746adc34bcf3544945942a4fcf17f2324e77d9d
Pete Hanner <petehanner@gmail.com>
2019-11-18 10:04:59 -0500

    setting up gems and columns

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ApplicationRecord
+  has_secure_password
+  validates :name, uniqueness: { case_sensitive: false }
+
+  validates :password,
+  length: { in: 8..20, wrong_length: 'Passwords must be between 8 and 20 characters' },
+  format: {with: /\A(?=.*[a-z])[a-z\d]+\Z/i}
+
   has_many :courses
   has_many :assignments, through: :courses
 
