921cea33b11226be55485a750076f8649296c2cd
Pete Hanner <petehanner@gmail.com>
2019-11-19 10:52:51 -0500

    proper error messages rendered

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,18 @@
 class User < ApplicationRecord
   has_secure_password
-  validates :name, uniqueness: { case_sensitive: false }
+  validates :name, uniqueness: {
+    case_sensitive: false,
+    message: 'is already taken.'
+  }
 
   validates :password,
   length: {
     in: 8..20,
-    wrong_length: 'Passwords must be between 8 and 20 characters'
+    message: 'must be between 8 and 20 characters'
   },
   format: {
     with: /\A(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}\z/,
-    message: 'Passwords must contain at least one letter, number, and special character.'
+    message: 'must contain at least one letter, number, and special character.'
   }
 
   has_many :courses
