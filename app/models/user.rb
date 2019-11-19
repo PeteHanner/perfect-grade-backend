2ded68b0d62b5147b871e2bad16a5ec1d45491c3
Pete Hanner <petehanner@gmail.com>
2019-11-19 08:36:49 -0500

    clear out unused code; further auth setup

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,15 +3,31 @@ class User < ApplicationRecord
   validates :name, uniqueness: { case_sensitive: false }
 
   validates :password,
-  length: { in: 8..20, wrong_length: 'Passwords must be between 8 and 20 characters' },
-  format: {with: /\A(?=.*[a-z])[a-z\d]+\Z/i}
+  length: {
+    in: 8..20,
+    wrong_length: 'Passwords must be between 8 and 20 characters'
+  },
+  format: {
+    with: /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/,
+    message: 'Passwords must contain at least one letter, number, and special character.'
+  }
 
   has_many :courses
   has_many :assignments, through: :courses
 
-  def flatten
-    workload = self.assignments
-    print workload
+  def create
+    @user = User.create(user_params)
+    if @user.valid?
+      render json: {user: UserSerializer.new(@user)}, status: :created
+    else
+      render json {error: 'failed to create user'}, status: :not_acceptable
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :password)
   end
 
 end
