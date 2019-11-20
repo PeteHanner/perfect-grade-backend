dc9ff7cccb4bcb289c2d8719687f07f1904baa73
Pete Hanner <petehanner@gmail.com>
2019-11-20 12:23:56 -0500

    prepping auth & users controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,13 +6,27 @@ class UsersController < ApplicationController
     render json: users
   end
 
+  def profile
+    render json: {
+             user: UserSerializer.new(current_user),
+           },
+           status: :accepted
+  end
+
   def create
     @user = User.create(user_params)
     if @user.valid?
       @token = encode_token(user_id: @user.id)
-      render json: { user: UserSerializer.new(@user), jwt: @token }, status: :created
+      render json: {
+               user: UserSerializer.new(@user),
+               jwt: @token,
+             },
+             status: :created
     else
-      render json: { error: @user.errors.full_messages }, status: :not_acceptable
+      render json: {
+               error: @user.errors.full_messages,
+             },
+             status: :not_acceptable
     end
   end
 
