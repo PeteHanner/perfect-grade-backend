36f11c4e10e8146358f282d2fd906c0a7b8526b2
Pete Hanner <petehanner@gmail.com>
2019-11-20 16:39:34 -0500

    more intuitive route setup

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,7 +1,7 @@
 class AuthController < ApplicationController
   skip_before_action :authorized, only: [:create]
 
-  def create
+  def login
     @user = User.find_by(name: user_login_params[:name])
 
     if @user && @user.authenticate(user_login_params[:password])
@@ -9,7 +9,7 @@ class AuthController < ApplicationController
       render json: { user: UserSerializer.new(@user), jwt: token },
              status: :accepted
     else
-      render json: { message: "Invalide username or password" },
+      render json: { message: "Invalid username or password" },
              status: :unauthorized
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
-  get '/assignments/first', to: 'assignments#first_request'
-  get '/assignments/fresh', to: 'assignments#fresh_request'
+  get "/assignments/first", to: "assignments#first_request"
+  get "/assignments/fresh", to: "assignments#fresh_request"
   resources :assignments, only: [:index, :create, :update, :destroy]
   resources :courses, only: [:index, :create, :update, :destroy]
   resources :users, only: [:create, :update, :destroy]
-  post '/login', to: 'auth#create'
+  post "/login", to: "auth#login"
 end
